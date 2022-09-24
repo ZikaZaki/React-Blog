cd1593d02b34d93c1aa8dcef478b1b420f972589
ZikaZaki <zika.github@gmail.com>
2022-09-25 01:19:39 +0300

    Add vid 14

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+import { useState } from 'react';
 import Navbar from './Navbar.js'
 import Home from './Home.js'
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
   return (
     <div className="App">
       <Navbar />
       <div className="content">
+        <h1>{counter}</h1>
         <Home />
       </div>
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'ZikaZaki', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'ZikaZaki', id: 3 }
+    ]);
+
+    const handleDelete = (id) => {
+        setBlogs(blogs.filter((blog) => blog.id !== id));
+    }
+
+    useEffect(()=>{
+        console.log('use effect ran'); 
+        console.log(blogs);
+    });
+ 
     return (
         <div className="home">
-            <h2>Homepage</h2>
+         <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+         {/* <BlogList blogs={blogs.filter((blog)=> blog.author==='ZikaZaki')} title="ZikaZaki's Blogs" /> */}
         </div>
     );
 }
