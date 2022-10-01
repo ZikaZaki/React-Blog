cbb7f03c84beb5c24fd42e702b374053d627cf06
ZikaZaki <zika.github@gmail.com>
2022-10-01 22:46:46 +0300

    Add create form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,69 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('ZikaZaki');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify(blog)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            setIsPending(false);
+            // history.go(1);
+            history.push('/');
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            setIsPending(false);
+        });
+    }
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    placeholder="write your blog title"
+                    required 
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    placeholder="write your blog body"
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                >
+                </textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="ZikaZaki">ZikaZaki</option>
+                    <option value="Mario">Mario</option>
+                    <option value="Yoshi">Yoshi</option>
+                </select>
+                { !isPending && <button>Add Blog</button> }
+                { isPending && <button disabled>Adding blog...</button> }
+            </form>
         </div>
      );
 }
